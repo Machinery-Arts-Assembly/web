fc43e46ba46421752fee6c325105137eb0c0694a
intervinn <69168328+intervinn@users.noreply.github.com>
2023-05-24 19:59:15 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center">
+        <h1 className="text-2xl font-sans text-black">It works</h1>
+    </div>
+  )
+}
+
+export default App
